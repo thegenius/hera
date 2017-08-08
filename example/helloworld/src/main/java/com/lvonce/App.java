b0c101986cc87c64e3b0ac59ecc7d2a4d4c1f988
Wang Wei <thegenius@vip.qq.com>
2017-08-08 13:28:52 +0800

    add example dir

---
+++ example/helloworld/src/main/java/com/lvonce/App.java
@@ -0,0 +1,37 @@
+package com.lvonce;
+import com.lvonce.hera.logger.RpcLogger;
+import com.lvonce.hera.future.RpcFuture;
+import com.lvonce.hera.netty.NettyRpcNode;
+import com.lvonce.hera.consumer.RpcConsumerFactory;
+
+public class App {
+
+	public static interface Service {
+		public String hello(String name);
+	}
+
+	public static class Provider implements Service {
+		public String hello(String name) {
+			return "Hello " + name;
+		}
+	}
+
+	public static void main(String[] args) {
+		if (args[0].equals("a")) {
+			NettyRpcNode.export(Service.class, new Provider());
+			NettyRpcNode.start(3721);
+		}
+
+		if (args[0].equals("b")) {
+			Service service = RpcConsumerFactory.create(
+				RpcConsumerFactory.Type.ASM_PROXY, 
+				Service.class, 
+				2000, 
+				"127.0.0.1", 
+				3721);
+
+			String result = service.hello("World!");
+			RpcLogger.info(App.class, result);
+		}
+	}
+}
